8bde62c82a9d0f111910e76a1e87e5b2471f6de5
Grinden <bce.gomel@gmail.com>
2022-12-12 10:21:15 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [HttpClientModule, BrowserModule, AppRoutingModule, BrowserAnimationsModule, CommonModule],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {}
